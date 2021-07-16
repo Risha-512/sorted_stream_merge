007e136c38aa12ed1757f23b97fe91ae674e88fe
Rishana125 <marinka.lagunova@gmail.com>
2021-07-17 01:25:29 +0700

    Decomprose sort method, update types

--- binary_heap.h
+++ binary_heap.h
@@ -4,66 +4,59 @@
 #include <vector>
 #include <utility>
 
-template <class T>
-class BinaryHeap
-{
+template<class T>
+struct heap_data {
+    int index;
+    T item;
+
+    heap_data(int index, T item) : index(index), item(item) {}
+};
+
+template<class T>
+class BinaryHeap {
 private:
     // pairs of template items and integer identifiers
-    std::vector<std::pair<int, T>> list;
+    std::vector<heap_data<T>> list;
 
 public:
-    [[nodiscard]] inline int get_heap_size() const
-    { return list.size(); }
+    [[nodiscard]] inline int get_heap_size() const { return list.size(); }
 
-    [[nodiscard]] inline std::pair<int, T> get_min() const
-    { return list.front(); }
+    [[nodiscard]] inline heap_data<T> get_min() const { return list.front(); }
 
     BinaryHeap() = default;
 
-    explicit BinaryHeap(std::vector<std::pair<int, T>> source)
-    {
-        list = source;
-        for (int i = get_heap_size() / 2; i >= 0; i--)
-            heapify(i);
-    }
-
-    void add (const std::pair<int, T> &item)
-    {
+    void add(heap_data<T> item) {
         list.emplace_back(item);
 
         int i = get_heap_size() - 1,
                 parent = (i - 1) / 2;
 
-        while (i > 0 && list[parent].second > list[i].second)
-        {
+        while (i > 0 && list[parent].item > list[i].item) {
             std::swap(list[i], list[parent]);
             i = parent;
             parent = (i - 1) / 2;
         }
     }
 
-    void heapify(int index)
-    {
+    void heapify(int index) {
         int left_child = 2 * index + 1;
         int right_child = 2 * index + 2;
         int smallest_child = index;
 
-        if (left_child < get_heap_size() && list[left_child].second < list[smallest_child].second)
+        if (left_child < get_heap_size() && list[left_child].item < list[smallest_child].item)
             smallest_child = left_child;
 
-        if (right_child < get_heap_size() && list[right_child].second < list[smallest_child].second)
+        if (right_child < get_heap_size() && list[right_child].item < list[smallest_child].item)
             smallest_child = right_child;
 
-        if (smallest_child != index)
-        {
+        if (smallest_child != index) {
             std::swap(list[index], list[smallest_child]);
             index = smallest_child;
             heapify(index);
         }
     }
 
-    std::pair<int, T> extract_min()
-    {
+    heap_data<T> extract_min() {
         auto result = get_min();
 
         list.front() = list.back();
--- main.cpp
+++ main.cpp
@@ -1,133 +1,153 @@
 #include <iostream>
-#include <fstream>
 #include <filesystem>
 #include <string>
 #include <cstring>
+#include <bits/stdc++.h>
 #include "binary_heap.h"
 
 namespace fs = std::filesystem;
 
-template <typename T>
-int sort(const char* output_path, const std::vector<char*> &input_paths, BinaryHeap<T> &heap, T template_min)
-{
+struct input_file_data {
+    fs::path path;
+    std::ifstream *stream;
+
+    input_file_data(fs::path &path, std::ifstream *stream) : path(path), stream(stream) {}
+};
+
+std::vector<input_file_data> get_input_files(const std::vector<char *> &input_paths) {
+    /* check and open all correct input files
+     * save file names for each stream (make vector of pairs of file names and streams) */
+
+    std::vector<input_file_data> input_files;
+
+    // skip invalid files
+    for (auto const &path : input_paths) {
+        fs::path in(path);
+
+        if (!fs::exists(in)) {
+            std::cerr << "Input file " << in << " doesn't exist!\n";
+            continue;
+        }
+        if (fs::is_empty(in)) {
+            std::cerr << "Input file " << in << " is empty!\n";
+            continue;
+        }
+        try {
+            input_files.emplace_back(input_file_data(in, new std::ifstream(path)));
+        }
+        catch (std::ios_base::failure &e) {
+            std::cerr << "Error opening file " << in
+                      << "\nExplanation: " << e.what() << "\n";
+            continue;
+        }
+    }
+
+    return input_files;
+}
+
+template<typename T>
+void push_item_to_heap(
+        input_file_data &file,
+        BinaryHeap<T> &heap,
+        T last_item,
+        int last_file_index,
+        int current_file_index
+) {
+    /* read data from file which element was the previous (last) minimum
+     * or read data from all files if file with the previous (last) minimum isn't available */
+
+    if (last_file_index >= 0 && last_file_index != current_file_index) return;
+
+    T current_item;
+    *file.stream >> current_item;
+
+    // check if the input is invalid and skip line with invalid data
+    while ((file.stream->fail() || current_item < last_item) && !file.stream->eof()) {
+        file.stream->clear();
+        std::cerr << "Invalid data in file " << file.path
+                  << "\nPosition " << file.stream->tellg() << "\n\n";
+
+        file.stream->ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        *file.stream >> current_item;
+    }
+
+    // add item to heap only if it's correct (it helps if the last data from this file is invalid)
+    // heap element contains template item and integer identifier
+    if (!file.stream->fail() && current_item >= last_item)
+        heap.add(heap_data<T>(current_file_index, current_item));
+}
+
+template<typename T>
+int sort(
+        const char *output_path,
+        const std::vector<char *> &input_paths,
+        BinaryHeap<T> &heap,
+        T template_min
+) {
     // check and open output file
     fs::path out(output_path);
     if (!fs::exists(out))
         std::cout << "Creating file " << out << "\n";
 
     std::ofstream output;
-    try
-    {
+    try {
         output.open(output_path, std::ios::out | std::ios::trunc);
     }
-    catch (std::ios_base::failure &e)
-    {
+    catch (std::ios_base::failure &e) {
         std::cerr << "Error opening file " << out
-                  << "\nError code: " << e.code() << "\n";
+                  << "\nExplanation: " << e.what() << "\n";
         return 1;
     }
 
-    // check and open all correct input files
-    // save file names for each stream (make vector of pairs of file names and streams)
-    std::vector< std::pair<fs::path, std::unique_ptr<std::ifstream> >> input_files;
-    for (auto const &path : input_paths)
-    {
-        // not push invalid files in vector
-        fs::path in(path);
-        if (!fs::exists(in))
-        {
-            std::cerr << "Input file " << in << " doesn't exist!\n";
-            continue;
-        }
-        if (fs::is_empty(in))
-        {
-            std::cerr << "Input file " << in << " is empty!\n";
-            continue;
-        }
-        try
-        {
-            std::unique_ptr<std::ifstream> input(new std::ifstream(path));
-            input_files.emplace_back(std::make_pair(in, std::move(input)));
-        }
-        catch (std::ios_base::failure &e)
-        {
-            std::cerr << "Error opening file " << in
-                      << "\nError code: " << e.code() << "\n";
-            continue;
-        }
-    }
+    auto input_files = get_input_files(input_paths);
 
-    if (input_files.empty())
-    {
+    if (input_files.empty()) {
         std::cerr << "All input files are empty or have an error with opening!\n";
         return 1;
     }
 
-    bool loop_continue = true;
-    T buf;
     // previous (last) minimum element
     T last_item = template_min;
-    // the index of the file from which the element was the previous (last) minimum
-    int last_index = -1;
 
-    while (loop_continue)
-    {
-        for (int i = 0; i < input_files.size(); i++)
-        {
+    // the index of the file from which the element was the previous (last) minimum
+    int last_file_index = -1;
+
+    bool loop_continue = true;
+
+    while (loop_continue) {
+        for (int i = 0; i < input_files.size(); i++) {
             // skip closed or finished files
-            if (!input_files[i].second->is_open()) continue;
-            if (input_files[i].second->eof())
-            {
-                input_files[i].second->close();
-                if (last_index == i)
-                    last_index = -1;
+            if (!input_files[i].stream->is_open()) {
                 continue;
             }
 
-            // read data from file which element was the previous (last) minimum
-            // or read data from all files if file with the previous (last) minimum isn't available
-            if (last_index < 0 || last_index == i)
-            {
-                *input_files[i].second >> buf;
-
-                // check if the input is invalid and skip line with invalid data
-                while ((input_files[i].second->fail() || buf < last_item) && !input_files[i].second->eof())
-                {
-                    input_files[i].second->clear();
-                    std::cerr << "Invalid data in file " << input_files[i].first
-                              << "\nPosition " << input_files[i].second->tellg() << "\n\n";
-
-                    input_files[i].second->ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                    *input_files[i].second >> buf;
-                }
-
-                // add item to heap only if it's correct (it helps if the last data from this file is invalid)
-                // heap element contains template item and integer identifier
-                if (!input_files[i].second->fail() && buf >= last_item) heap.add(std::make_pair(i, buf));
+            if (input_files[i].stream->eof()) {
+                input_files[i].stream->close();
+                if (last_file_index == i)
+                    last_file_index = -1;
+                continue;
             }
+
+            push_item_to_heap(input_files[i], heap, last_item, last_file_index, i);
         }
 
         if (!heap.get_heap_size()) break;
 
         // write minimum to output
         auto min = heap.extract_min();
-        last_index = min.first;
-        last_item = min.second;
+        last_file_index = min.index;
+        last_item = min.item;
         output << last_item << '\n';
 
         // continue loop if there is a file which haven't been read
-        loop_continue = [&input_files]
-        {
-            for (auto const &item : input_files)
-                if (!item.second->eof())
-                    return true;
-            return false;
+        loop_continue = [&input_files] {
+            return std::ranges::any_of(input_files, [](auto const &file) { return !file.stream->eof(); });
         }();
     }
 
     // get all remaining elements
-    while(heap.get_heap_size())
-        output << heap.extract_min().second << '\n';
+    while (heap.get_heap_size())
+        output << heap.extract_min().item << '\n';
 
     output.close();
     std::cout << "Finished!\n";
@@ -135,29 +155,22 @@ int sort(const char* output_path, const std::vector<char*> &input_paths, BinaryH
     return 0;
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 4)
-    {
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
         std::cerr << "Not enough parameters. Usage: sort.exe [-s | -i] [output.txt] [input1.txt ...]";
         return 1;
     }
 
     // vector of input files paths
-    std::vector<char*> input_paths(argv + 3, argv + argc );
+    std::vector<char *> input_paths(argv + 3, argv + argc);
 
-    if (!strcmp(argv[1], "-i"))
-    {
+    if (!strcmp(argv[1], "-i")) {
         BinaryHeap<int> heap;
         return sort(argv[2], input_paths, heap, INT_MIN);
-    }
-    else if (!strcmp(argv[1], "-s"))
-    {
+    } else if (!strcmp(argv[1], "-s")) {
         BinaryHeap<std::string> heap;
         return sort(argv[2], input_paths, heap, std::string(""));
-    }
-    else
-    {
+    } else {
         std::cerr << R"(First parameter must be '-s' for files with strings or '-i' for files with integers)";
         return 1;
     }
